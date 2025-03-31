ae200918eff043b9c2f1c6f8c8299f7d6c129f82
Ap00rvx <apurvabraj419@gmail.com>
2025-03-31 09:40:06 +0530

    get user details geet routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.post("/authenticate",authenticate);
 router.post("/verify-otp",verifyOtp);
 router.post("/save",authenticateUser,saveUserDetails);
-router.post("/",authenticateUser,getUserDetails);
+router.get("/",authenticateUser,getUserDetails);
 router.post("/save-fcm-token",authenticateUser,saveFCMToken);
 
 export default router;
